191f2ae9dbcb3c4fe73f1954b252d360d68fab0f
Marina Vyshnevska <Marinessa393@users.noreply.github.com>
2021-01-05 00:00:44 +0200

    done

---
+++ src/js/getRefs.js
@@ -0,0 +1,6 @@
+const searchForm = document.querySelector(".itemSearch");
+const countriesContainer = document.querySelector(".countryGallery");
+export default {
+  searchForm,
+  countriesContainer,
+};
